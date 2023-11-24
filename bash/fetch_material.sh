a901ee19deb3d5390d1cb468b899e62c133b00da
Peter von Rohr <peter.vonrohr@qualitasag.ch>
2023-11-24 16:56:17 +0100

    Updated paths to released material

--- bash/fetch_material.sh
+++ bash/fetch_material.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # sq2h
 SCRIPTDIR=/home/quagadmin/courses/lbgfs2023/bash
-#DIRTOFETCH=ex/lbg_ex09
-DIRTOFETCH=sol/lbg_ex08
+DIRTOFETCH=ex/lbg_ex11
+#DIRTOFETCH=sol/lbg_ex09
 TESTSTUDENT=/home/quagadmin/courses/lbgfs2023/students/test_usernames_lbgfs_2023.csv
 STUDENTUSER=/home/quagadmin/courses/lbgfs2023/students/usernames_lbgfs_2023.csv
 #' fetch individual directory
